c8582a700a1b1aa339f4ac396658a5bfa07aa1b0
marcelonuness <marcelohenrique.nunes@gmail.com>
2023-09-07 13:16:24 -0300

    commit2

--- src/app/produtos/page.js
+++ src/app/produtos/page.js
@@ -1,7 +1,15 @@
+import SideBar from "@/components/navbar/side-bar";
+
 export default function Produtos() {
     return(
-        <div>
-            
-        </div>
+        <div className="h-screen flex flex-col mt-[87px]">
+                <div className="flex flex-1 border">
+                    <div className='fixed'>
+                        <SideBar></SideBar>
+                    </div>
+                    <div className='w-full flex flex-col items-center p-2 bg-scroll ml-64'>
+                    </div>
+                </div>
+            </div>
     )
 }
---
+++ src/components/navbar/side-bar.js
@@ -0,0 +1,15 @@
+export default function SideBar() {
+    return (
+        <aside className="h-screen w-64 shadow-2xl">
+            <ul>
+                <li>Clp&apos;s</li>
+                <li>Fontes</li>
+                <li>IHM&apos;s</li>
+                <li>Brio&apos;s</li>
+                <li>Cabos</li>
+                <li>Semi-novos</li>
+                <li>Produtos Antigos</li>
+            </ul>
+        </aside>
+    )
+}
