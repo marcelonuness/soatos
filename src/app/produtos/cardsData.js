0054e3d8afa76653c90191a74dc566d6971947ed
marcelonuness <marcelohenrique.nunes@gmail.com>
2023-11-24 15:58:08 -0300

    aaaa3

--- src/app/produtos/cardsData.js
+++ src/app/produtos/cardsData.js
@@ -42,4 +42,148 @@ export const cardsData = [
         imageUrl: "/photo7.png",
         category: "Clps"
     },
+    {
+        title: "4004.09R CONTROLADOR PROGRAMAVEL MPC4004 XA C/BATERIA 8E/S RELE",
+        description: "",
+        imageUrl: "/photo8.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.62/P MODULO EXPANSAO ANALOGICA P/MPC4004 4 ENTRADAS 0-10VCC ",
+        description: "",
+        imageUrl: "/photo9.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.06R CONTROLADOR PROGRAMAVEL MPC4004 XA RAM BATERIA 8E/S(PNP) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.31H MODULO EXPANSAO DIGITAL P/MPC4004 16 SAIDAS (NPN) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.45 MODULO DE ENERGIA",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.53H MODULO EXPANSAO DIGITAL P/MPC4004 16E (NPN/PNP) 16S NPN ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.73 MODULO EXPANSAO AMPLIFICADOR P/MPC4004 PARA VALVULA 4 CANAIS ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.02 CONTROLADOR PROGRAMAVEL MPC4004 NVRAM 8 E/S (PNP) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.74GB MODEM CEL. GSM/GPRS+CLP MODBUS",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.53 MODULO EXPANSAO DIGITAL P/MPC4004 16 E/S (NPN) 24VCC ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.05R CONTROLADOR PROGRAMAVEL MPC4004 XA RAM BATERIA 8E/S(NPN) 24VCC ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.73M MODULO EXPANSAO AMPLIFICADOR P/MPC4004 PARA VALVULA 2 CANAIS ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.57 MODULO EXPANSAO DIGITAL P/MPC4004 8E (PNP/NPN) 8S RELE",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.76P MODULO EXPANSAO TEMPERATURA P/MPC4004 8E PT100 -50-50 GRAUS",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.78 MODULO CONVERSOR P/MPC4004 ETHERNET PARA SERIAL",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.85 MODULO EXPANSAO TEMPERATURA P/MPC4004 4E PT100+4E ANALOGICA",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.31G MODULO EXPANSAO DIGITAL P/MPC4004 16 SAIDAS (NPN) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.34G MODULO EXPANSAO DIGITAL P/MPC4004 16 ENTRADAS (PNP) 24VCC ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.39 MODULO EXPANSAO DIGITAL P/MPC4004 8 SAIDAS TRIAC 90-240VCA",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.03 CONTROLADOR PROGRAMAVEL MPC4004 NVRAM 8 E/S (NPN) 24VCC ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.51 MODULO EXPANSAO DIGITAL P/MPC4004 8 E/S (NPN) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.33G MODULO EXPANSAO DIGITAL P/MPC4004 16 ENTRADAS (NPN) 24VCC ",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.52 MODULO EXPANSAO DIGITAL P/MPC4004 8 E/S (PNP) 24VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
+    {
+        title: "4004.61N MODULO EXPANSAO ANALOGICA P/MPC4004 4 E/S +/-10VCC",
+        description: "",
+        imageUrl: "/photo.png",
+        category: "Clps"
+    },
 ];
